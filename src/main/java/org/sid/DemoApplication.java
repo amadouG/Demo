c8e31bcc0d16b4e4b10fb388dd3fcf51a25d814c
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2019-11-28 11:18:45 +0000

    correction

--- src/main/java/org/sid/DemoApplication.java
+++ src/main/java/org/sid/DemoApplication.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 
 import org.sid.dao.ContactRepository;
 import org.sid.entities.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner{
 
+	@Autowired
 	ContactRepository contactRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
