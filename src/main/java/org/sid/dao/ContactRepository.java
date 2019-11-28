26c8ad316ff745f151350e5917b867e32c4a5e35
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2019-11-28 11:26:05 +0000

    correction

--- src/main/java/org/sid/dao/ContactRepository.java
+++ src/main/java/org/sid/dao/ContactRepository.java
@@ -2,7 +2,9 @@ package org.sid.dao;
 
 import org.sid.entities.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, Long>
 {
 
